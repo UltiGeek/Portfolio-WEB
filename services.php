89a7d04fbd459c63b86edf90a2d0685130c4e267
UltiGeek <feisal@yoteyote.com>
2015-02-18 21:23:31 +0300

    added new tabs for extra services page :punch:

--- services.php
+++ services.php
@@ -13,11 +13,11 @@
                 <div class="page-title">
                 	<div class="page-title-detail">
                     	<h1>Our services</h1>
-                        <p class="sub-title">Sed diam nonumy eirmod tempor invidunt</p>
+                        <p class="sub-title">Let Cute Design add life to your Business</p>
                     </div>
                     <nav class="page-navigation">
                     	<ul>
-                        	<li><a href="index.html">home</a></li>
+                        	<li><a href="index.php">home</a></li>
                             <li>our services</li>
                         </ul>
                     </nav>
@@ -29,7 +29,7 @@
                             <p class="text-content"><span class="dropcap">G</span>onsetetur sadipscing elitr sed diam nonumy eirmod tempor labore et dolore magna aliquyam erat sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit sea takimata sanctus est. At vero eos et accusam et justo duo dolores et east rebum. Stet kasd dolores gubergren takimata sanctus est lorem ipsum dolor sit amet. dolor sit amet consetetur sadipscing elitr sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.</p>
                             <ul class="service-list">
                             	
-                                <!--Service 1-->
+                                <!--Printing Service-->
                                 <li>
                                 	<a href="#">
                                         <figure>
@@ -38,13 +38,13 @@
                                         </figure>
                                     </a>
                                     <div class="service-detail">
-                                    	<h2>Lorem ipsum dolor sit amet</h2>
-                                        <p>At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore.</p>
-                                        <button class="dark-button" onclick="location.href='#'">More Details</button>
+                                    	<h2>Printing</h2>
+                                        <p>Our top most priority is highest perfection and quality with taste. We print all promotional items eg T-shirts, Shirts, calendars, Rollup banners, brochures, posters, vehicle branding , wheelcovers, billboards, caps,  and  banners. We also print packaging items, annual reports, books, magazines, self adhesive Labels (in flexo or Paper material), fliers, folders etc. We let our customers negotiate the price but not quality. Special unique finishing effects available in paper printing ask our marketing personels for details and samples.   </p>
+                                        <button class="dark-button" onclick="location.href='#'">See Our Printing Jobs</button>
                                     </div>
                                 </li>
                                 
-                                <!--Service 2-->
+                                <!--Website Design part -->
                                 <li>
                                 	<a href="#">
                                         <figure>
@@ -53,13 +53,13 @@
                                         </figure>
                                     </a>
                                     <div class="service-detail">
-                                    	<h2>Sed diam nonumy eirmod</h2>
-                                        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren sea takimata.</p>
-                                        <button class="dark-button" onclick="location.href='#'">More Details</button>
+                                    	<h2>Web Design</h2>
+                                        <p>We do domain registration, web designing, updating, developing, publishing and  online advertising at very competitive and affordable rates.</p>
+                                        <button class="dark-button" onclick="location.href='#'">See Examples</button>
                                     </div>
                                 </li>
                                 
-                                <!--Service 3-->
+                                <!--Advertising part-->
                                 <li>
                                 	<a href="#">
                                         <figure>
@@ -68,13 +68,13 @@
                                         </figure>
                                     </a>
                                     <div class="service-detail">
-                                    	<h2>Stet clita kasd gubergren</h2>
-                                        <p>Takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd.</p>
+                                    	<h2>Advertising and Branding</h2>
+                                        <p>From launching a new brand to promoting an existing one, we help our clients communicate a clear point of difference to strengthen their brand’s presence in the market place. In every department our team is packed with skilled and experienced staff, they work collaboratively bringing a wealth of knowledge and experience to every project and deliver something that will get users attention and will have an impressive visual. Come see us for TV Commercials, audio and print adverts eg billboard, newspaper etc. </p>
                                         <button class="dark-button" onclick="location.href='#'">More Details</button>
                                     </div>
                                 </li>
                                 
-                                <!--Service 4-->
+                                <!--Graphics-->
                                 <li>
                                 	<a href="#">
                                         <figure>
@@ -83,8 +83,40 @@
                                         </figure>
                                     </a>
                                     <div class="service-detail">
-                                    	<h2>Accusam justo duo dolores</h2>
-                                        <p>Sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+                                    	<h2>Graphics Designing</h2>
+                                        <p>Graphic designing requires a combination of computer skills and creative ability. At CUTE DESIGN we produce effective materials from concept through final production. Whether what you need is a book, brochure, billboard, vehicle branding, annual report, a logo or a complete corporate identity package, we can meet your design needs and deliver your expectations to satisfy the requirements of your company.
+                                        </p>
+                                        <button class="dark-button" onclick="location.href='#'">More Details</button>
+                                    </div>
+                                </li>
+                                <!--photography-->
+                                <li>
+                                    <a href="#">
+                                        <figure>
+                                            <div class="thumbnail-hover"></div>
+                                            <img src="images/upload/service-thumbnail4.jpg" alt="our service">
+                                        </figure>
+                                    </a>
+                                    <div class="service-detail">
+                                        <h2>Photography</h2>
+                                        <p>Why downloading pictures from the web while you can have yours taken the way you want? Protect your company from the risk of being prosecuted by using copyrighted pictures from the internet and other sources as is hard to know which one is not copyrighted.Our Professional Photographers, with the help of modern cameras and softwares, will take pictures for you and burn on CD or DVD ready for use anytime you want.We can add effects to your images or do graphics on them. And if you need models, here you are, …we have plenty of them and of different ages and gender. </p>
+                                        <button class="dark-button" onclick="location.href='#'">More Details</button>
+                                    </div>
+                                </li>
+                                <!--photography-->
+                                <li>
+                                    <a href="#">
+                                        <figure>
+                                            <div class="thumbnail-hover"></div>
+                                            <img src="images/upload/service-thumbnail4.jpg" alt="our service">
+                                        </figure>
+                                    </a>
+                                    <div class="service-detail">
+                                        <h2>Cartoons</h2>
+                                        <p>CUTE DESIGN is staffed with talented artists who are good in illustrations for any project and rich in humour. If you are looking for humour, educational or political cartoons for your publications, project etc., sit back and relax as the rest will be our business. We do hand-drawing and computer drawing - the choice is all yours.
+
+                                            We draw cartoons and illustrations for every project: Packaging, promotional materials, advertising, educational books and materials book covers, magazine etc.
+                                        </p>
                                         <button class="dark-button" onclick="location.href='#'">More Details</button>
                                     </div>
                                 </li>
