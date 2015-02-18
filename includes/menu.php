af33f3d6c2a6594e7211f50dfc14b1e722e512a3
Fahad Kassim <fahad@yoteyote.com>
2015-02-19 00:05:21 +0200

    Pages are active for current page :airplane:

--- includes/menu.php
+++ includes/menu.php
@@ -14,25 +14,25 @@
 
         <!--Navigation Menu Start-->
         <ul class="flexy-menu vertical">
-            <li class="active" style="display: block;"><a href="index.php">Home</a><div class="menu-space"></div>
+            <li class="<?php echo ($active=="index")?'active':'';?>" style="display: block;"><a href="index.php">Home</a><div class="menu-space"></div>
                 <span class="indicator">+</span></li>
-            <li style="display: block;"><a href="about-us.php">About Us</a><div class="menu-space"></div>
+            <li class="<?php echo ($active=="about")?'active':'';?>" style="display: block;"><a href="about-us.php">About Us</a><div class="menu-space"></div>
 
                 <span class="indicator">+</span></li>
-            <li style="display: block;"><a href="services.php">Our Services</a><div class="menu-space"></div>
+            <li class="<?php echo ($active=="services")?'active':'';?>" style="display: block;"><a href="services.php">Our Services</a><div class="menu-space"></div>
 
                 <span class="indicator">+</span></li>
-            <li style="display: block;"><a href="portfolio.php">Portfolio</a><div class="menu-space"></div>
+            <li class="<?php echo ($active=="portfolio")?'active':'';?>" style="display: block;"><a href="portfolio.php">Portfolio</a><div class="menu-space"></div>
 
                 <span class="indicator">+</span></li>
-            <li style="display: block;"><a href="#">Blog</a><div class="menu-space"></div>
+            <li class="<?php echo ($active=="blog")?'active':'';?>" style="display: block;"><a href="#">Blog</a><div class="menu-space"></div>
 
                 <span class="indicator">+</span></li>
-            <li style="display: block;"><a href="default_page.php">Page</a><div class="menu-space"></div>
+            <li class="<?php echo ($active=="default")?'active':'';?>" style="display: block;"><a href="default_page.php">Page</a><div class="menu-space"></div>
 
                 <span class="indicator">+</span></li>
-            <li style="display: block;"><a href="clients.php">Clients</a><div class="menu-space"></div></li>
-            <li style="display: block;"><a href="contact-us.php">Contact Us</a><div class="menu-space"></div></li>
+            <li class="<?php echo ($active=="clients")?'active':'';?>" style="display: block;"><a href="clients.php">Clients</a><div class="menu-space"></div></li>
+            <li class="<?php echo ($active=="contact")?'active':'';?>" style="display: block;"><a href="contact-us.php">Contact Us</a><div class="menu-space"></div></li>
         </ul>
         <!--Navigation Menu End-->
 
